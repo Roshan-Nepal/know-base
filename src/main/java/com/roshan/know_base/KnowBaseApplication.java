78aaaf0a0d7513b76e992951e7bf0e7e074f67a3
Roshan Nepal <roshan.n9959@gmail.com>
2026-05-19 18:54:35 +0545

    Initial commit

---
+++ src/main/java/com/roshan/know_base/KnowBaseApplication.java
@@ -0,0 +1,13 @@
+package com.roshan.know_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowBaseApplication.class, args);
+	}
+
+}
