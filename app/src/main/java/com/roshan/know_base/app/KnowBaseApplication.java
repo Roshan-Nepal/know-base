b2372437d52ca4399249dd8d380887a7a005dcef
Roshan Nepal <roshan.n9959@gmail.com>
2026-05-20 19:43:23 +0545

    add multimodule project structure

--- app/src/main/java/com/roshan/know_base/app/KnowBaseApplication.java
+++ app/src/main/java/com/roshan/know_base/app/KnowBaseApplication.java
@@ -1,13 +1,11 @@
-package com.roshan.know_base;
+package com.roshan.know_base.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KnowBaseApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KnowBaseApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(KnowBaseApplication.class, args);
+    }
 }
