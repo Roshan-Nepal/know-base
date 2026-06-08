5e345de80fe86b3e073b7d1f4066797447dbc4c9
Roshan Nepal <roshan.n9959@gmail.com>
2026-06-08 18:53:41 +0545

    feat: add async configuration

---
+++ infrastructure/src/main/java/com/roshan/know_base/infrastructure/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.roshan.know_base.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
