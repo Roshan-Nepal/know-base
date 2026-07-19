bac16aa4a5a367b53c4a1ad1e492d803b3032b12
Roshan Nepal <roshan.n9959@gmail.com>
2026-07-19 17:02:16 +0545

    fix: add UUID generation strategy to entity id

--- document/src/main/java/com/roshan/know_base/document/entity/DocumentContent.java
+++ document/src/main/java/com/roshan/know_base/document/entity/DocumentContent.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 public class DocumentContent {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
