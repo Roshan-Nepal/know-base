b9e51360491a2063909498c3a523adb551875be3
Roshan Nepal <roshan.n9959@gmail.com>
2026-08-01 19:08:11 +0545

    feat(document): update document response to include tags and use detailed response dto

--- document/src/main/java/com/roshan/know_base/document/dto/DocumentDetailResponse.java
+++ document/src/main/java/com/roshan/know_base/document/dto/DocumentDetailResponse.java
@@ -1,19 +1,17 @@
 package com.roshan.know_base.document.dto;
 
-import com.roshan.know_base.document.entity.DocumentContent;
-import com.roshan.know_base.document.entity.Tag;
-
 import java.time.LocalDateTime;
-import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
-public record DocumentResponse(
+public record DocumentDetailResponse (
         UUID id,
         String name,
         String type,
         String status,
         Long fileSize,
         LocalDateTime createdAt,
+        String content,
         Set<TagResponse> tags
-){}
+){
+}
